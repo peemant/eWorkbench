079441d45a793ffe42cd6a771efd6aa9eb6ef604
peemant <space.time.traveller@gmail.com>
2024-09-04 17:08:12 -0400

    Completed the project search and display. Can now click projects in the list

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import moment from "moment";
+
 
 const app = express();
 const data = new pg.Client({
@@ -35,6 +37,11 @@ app.get("/projectManager", async (req, res) => {
     res.render("projectManager.ejs", {leftBar_menu: "leftBar_searchProject.ejs"});
 });
 
+app.get("/project/:project_number", async (req, res) => {
+    var p = await data.query(`SELECT * FROM public.projects WHERE projectnumber = '${req.params.project_number}'`);
+    res.render("projectManager.ejs", {leftBar_menu: "leftBar_projectLifeCycle.ejs", project: p});
+});
+
 app.get("/tools", async (req, res) => {
     res.render("tools.ejs", {leftBar_menu: "leftBar_tools.ejs"});
 });
@@ -48,6 +55,9 @@ app.get("/openProject", async (req, res) => {
 });
 
 app.post("/searchProject", async (req, res) => {
+
+    // Variables
+
     var sqlQueryString = "";
     var sqlStringArray = [];
     var sqlRequiredColumns = [];
@@ -58,6 +68,24 @@ app.post("/searchProject", async (req, res) => {
     var sqlWHERE_my = "";
 
     // Get the fields that have data inputed as a search filter
+
+    /*  The 5 fields are grabbed from "card_newProject.ejs"
+        I check each field if it's empty or has default values.
+        The 5 fields are
+
+        "project-name-input"
+        "sop-input"
+        "platform-selection"
+        "model-selection"
+        "my-selection"
+
+        Each time, I check if the "sqlQueryString" is empty. If it's empty
+        this means it's going to be the first item in the list. This means
+        I add a "WHERE" to the search and append the colomn name.
+        If it's not the first, I add " AND " before adding itself
+        to the query string.
+    */
+
     if (req.body["project-name-input"] !== "") {
         sqlStringArray.push(req.body["project-name-input"]);
         sqlRequiredColumns.push("projectname");
@@ -106,7 +134,7 @@ app.post("/searchProject", async (req, res) => {
     if (req.body["my-selection"] !== "default" ) {
         sqlStringArray.push(req.body["my-selection"]);
         sqlRequiredColumns.push("projectmodelyear");
-        sqlWHERE_my = "projectmodelyear >= " + "'" + req.body["my-selection"] + "'";
+        sqlWHERE_my = "projectmodelyear = " + "'" + req.body["my-selection"] + "'";
 
         if (sqlQueryString === "") {
             sqlQueryString = " WHERE " + sqlWHERE_my;
@@ -121,14 +149,34 @@ app.post("/searchProject", async (req, res) => {
 
     console.log(searchData.rows);
 
-    // Output the Data
+    // Output the data
+
+    // This is required to format the date recieved from SQL. I am using "moment" import package
+    // to format the date from 2027-06-16T04:00:00.000Z to DD-MM-YYYY. You can change this format
+    // below. I simply run through all the searchData I got from SQL and correct the date. It is
+    // required to show correctly in the HTML
+
+    searchData.rows.forEach(element => {
+        element.startofproduction = moment(element.startofproduction).utc().format("DD-MM-YYYY");
+    });
+
     sqlQueryString = "";
-    res.render("projectManager.ejs", {toLoad: "card_searchProject.ejs", leftBar_menu: "leftBar_searchProject.ejs", loadedProject: "projectList.ejs"});
+
+    // Renders the EJS. It loads the information in the "projectManager.ejs" adding in the "leftBar"
+    // and the "topNavBar.ejs". The search box is loaded from "card_searchProject.ejs".
+    // The data is then sent via "searchData"
+
+    res.render("projectManager.ejs", {
+        toLoad: "card_searchProject.ejs",
+        leftBar_menu: "leftBar_searchProject.ejs",
+        loadedProject: "projectList.ejs",
+        searchData: searchData.rows
+    });
 });
 
 app.get("/saveProject", async (req, res) => {
     res.render("projectManager.ejs", {leftBar_menu: "leftBar_searchProject.ejs"});
-});
+}); 
 app.post("/saveProject", async (req, res) => {
 
     // Grab the data from the form
